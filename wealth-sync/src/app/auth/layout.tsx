3b0f6cc3bb49976bc87d11f087f86dd6b2de88e0
BoyanK95 <boyan_k_95@abv.bg>
2025-04-05 22:47:28 +0300

    fix: styles add paragraph

--- wealth-sync/src/app/auth/layout.tsx
+++ wealth-sync/src/app/auth/layout.tsx
@@ -6,9 +6,9 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <main className="flex-1 flex">
-        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gradient-to-b from-[#026d32] to-[#15162c]">
+    <div className="flex min-h-screen flex-col">
+      <main className="flex flex-1">
+        <div className="flex min-h-screen flex-1 flex-col justify-center bg-gradient-to-b from-[#026d32] to-[#15162c] px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <div className="flex justify-center text-2xl font-bold tracking-tight text-white">
               <Link href="/" className="flex items-center gap-2">
@@ -16,6 +16,7 @@ export default function AuthLayout({
                 <span className="text-[hsl(128,35%,78%)]">Sync</span>
               </Link>
             </div>
+            <p className="mt-2 text-center bold shadow-2xl text-xl text-emerald-200">The calculator for your financial future.</p>
           </div>
 
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -25,7 +26,8 @@ export default function AuthLayout({
           <div className="mt-10 text-center text-white">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;Take charge of your financial future with Wealth Sync - where smart money management meets simplicity.&rdquo;
+                &ldquo;Take charge of your financial future with Wealth Sync -
+                where smart money management meets simplicity.&rdquo;
               </p>
             </blockquote>
           </div>
