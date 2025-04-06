6c6d7bdfbfe292f2d4f5e366d3d6baf4bf5f78bf
BoyanK95 <boyan_k_95@abv.bg>
2025-04-06 18:32:33 +0300

    feat: change ContactFooter based on user

--- wealth-sync/src/components/AboutPageComponents/ContactFooter.tsx
+++ wealth-sync/src/components/AboutPageComponents/ContactFooter.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { auth } from "@/server/auth";
+import Link from "next/link";
+import { Routes } from "@/lib/constants/routes";
 
-const ContactFooter = () => {
+const ContactFooter = async () => {
+  const session = await auth();
+
   return (
     <div className="mt-8 space-y-4 rounded-lg border border-green-100 bg-green-50 p-6 text-center">
       <h3 className="text-xl font-bold text-green-800">
@@ -11,9 +16,15 @@ const ContactFooter = () => {
         WealthSync brings all your investments together in one place, giving you
         the complete picture of your financial portfolio.
       </p>
-      <Button className="bg-green-700 hover:bg-green-800">
-        Get Started with WealthSync
-      </Button>
+      {session?.user ? (
+        <Button className="bg-green-700 hover:bg-green-800">
+          <Link href={Routes.DASHBOARD}>Go to Dashboard</Link>
+        </Button>
+      ) : (
+        <Button className="bg-green-700 hover:bg-green-800">
+          <Link href={Routes.LOGIN}>Get Started with WealthSync</Link>
+        </Button>
+      )}
     </div>
   );
 };
