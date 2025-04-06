39c066d3df15a97b1c67c0b57dccc1d5590448b5
BoyanK95 <boyan_k_95@abv.bg>
2025-04-06 18:38:26 +0300

    feat: create init PricingPage

---
+++ wealth-sync/src/app/pricing/page.tsx
@@ -0,0 +1,10 @@
+import PricingSection from '@/components/PricingSection'
+import React from 'react'
+
+const PricingPage = () => {
+  return (
+    <PricingSection />
+  )
+}
+
+export default PricingPage
