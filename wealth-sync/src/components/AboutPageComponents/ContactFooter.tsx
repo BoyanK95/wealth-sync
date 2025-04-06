9f0c605c45364013b2326336f354d3950e629b9a
BoyanK95 <boyan_k_95@abv.bg>
2025-04-07 01:34:54 +0300

    style add dark styles to ActionSection and ContactFooter

--- wealth-sync/src/components/AboutPageComponents/ContactFooter.tsx
+++ wealth-sync/src/components/AboutPageComponents/ContactFooter.tsx
@@ -8,16 +8,16 @@ const ContactFooter = async () => {
   const session = await auth();
 
   return (
-    <div className="mt-8 space-y-4 rounded-lg border border-green-100 bg-green-50 p-6 text-center">
-      <h3 className="text-xl font-bold text-green-800">
+    <div className="mt-8 space-y-4 rounded-lg border border-green-100 bg-green-50 p-6 text-center dark:bg-green-800">
+      <h3 className="text-xl font-bold text-green-800 dark:text-gray-100">
         Ready to simplify your investment tracking?
       </h3>
-      <p className="mx-auto max-w-2xl text-green-700">
+      <p className="mx-auto max-w-2xl text-green-700 dark:text-gray-300">
         WealthSync brings all your investments together in one place, giving you
         the complete picture of your financial portfolio.
       </p>
       {session?.user ? (
-        <Button className="bg-green-700 hover:bg-green-800">
+        <Button className="bg-green-700 hover:bg-green-800 dark:bg-gray-200 dark:hover:text-white dark:hover:bg-green-900 border border-white ">
           <Link href={Routes.DASHBOARD}>Go to Dashboard</Link>
         </Button>
       ) : (
