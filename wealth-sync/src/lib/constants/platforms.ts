95f0d81e8de2339cdce7e9c9d1e377c636621860
BoyanK95 <boyan_k_95@abv.bg>
2025-04-22 23:47:43 +0300

    feat: add etoro logo

--- wealth-sync/src/lib/constants/platforms.ts
+++ wealth-sync/src/lib/constants/platforms.ts
@@ -5,9 +5,13 @@ export interface Platform {
 }
 
 export const Platforms: Platform[] = [
-    //TODO add more platforms
-  { id: "trading212", name: "Trading212", logo: "/platforms/trading-212-icon.png" },
-  { id: "etoro", name: "eToro" },
+  //TODO add more platforms
+  {
+    id: "trading212",
+    name: "Trading212",
+    logo: "/platforms/trading-212-icon.png",
+  },
+  { id: "etoro", name: "eToro", logo: "/platforms/etoro-logo-icon.png" },
   { id: "binance", name: "Binance" },
   { id: "interactive_brokers", name: "Interactive Brokers" },
   { id: "takione", name: "Takion" },
