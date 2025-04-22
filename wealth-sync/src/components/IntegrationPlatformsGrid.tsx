66c5956896997c1d300ed283105a2fd13129cebf
BoyanK95 <boyan_k_95@abv.bg>
2025-04-23 00:18:52 +0300

    feat: add Link to platforms

--- wealth-sync/src/components/IntegrationPlatformsGrid.tsx
+++ wealth-sync/src/components/IntegrationPlatformsGrid.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Platforms } from "@/lib/constants/platforms";
 import Image from "next/image";
+import Link from "next/link";
 
 const IntegrationPlatformsGrid = () => {
   const displayedPlatforms = Platforms.slice(0, 6);
@@ -8,22 +9,28 @@ const IntegrationPlatformsGrid = () => {
   return (
     <div className="mx-auto grid max-w-5xl grid-cols-2 gap-6 py-12 md:grid-cols-3 lg:grid-cols-6">
       {displayedPlatforms.map((platform) => (
-        <div
+        <Link
+          href={platform.connetUrl ?? "/"}
           key={platform.id}
-          className="flex flex-col items-center space-y-2 rounded-lg border p-6"
+          className="group relative flex flex-col items-center space-y-2 rounded-lg border p-6 transition-all hover:border-green-700 hover:shadow-md"
         >
-          <div className="bg-muted h-12 w-12 rounded-full">
-            {platform.logo && (
-              <Image
-                src={platform.logo}
-                alt={platform.name}
-                width={48}
-                height={48}
-              />
-            )}
+          <div
+            key={platform.id}
+            className="flex flex-col items-center space-y-2 rounded-lg border p-6"
+          >
+            <div className="bg-muted h-12 w-12 rounded-full">
+              {platform.logo && (
+                <Image
+                  src={platform.logo}
+                  alt={platform.name}
+                  width={48}
+                  height={48}
+                />
+              )}
+            </div>
+            <span className="text-sm font-medium">{platform.name}</span>
           </div>
-          <span className="text-sm font-medium">{platform.name}</span>
-        </div>
+        </Link>
       ))}
       {
         //TODO make dynamic show more button
