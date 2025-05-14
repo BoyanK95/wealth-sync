c2daccac15108bc32419db14a1c37c3c081b17a2
BoyanK95 <boyan_k_95@abv.bg>
2025-05-14 10:02:54 +0300

    feat: create PortfolioValue with tooltip

--- wealth-sync/src/components/Dashboard/PortfolioValue/PortfolioValue.tsx
+++ wealth-sync/src/components/Dashboard/PortfolioValue/PortfolioValue.tsx
@@ -7,25 +7,31 @@ import {
 
 const PortfolioValue = ({ totalValue }: { totalValue: number }) => {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <div className="space-y-2">
-            <p className="text-muted-foreground text-sm">Portfolio Value</p>
-            <p className="text-2xl font-bold">
-              $
-              {totalValue.toLocaleString(undefined, {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2,
-              })}
-            </p>
-          </div>
-        </TooltipTrigger>
-        <TooltipContent className="dark:bg-background dark:text-foreground">
-          <p>Total value of all your open positions.</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="ml-15 inline-block cursor-default space-y-2">
+              <p className="text-muted-foreground text-sm">Portfolio Value</p>
+              <p className="text-2xl font-bold">
+                $
+                {totalValue.toLocaleString(undefined, {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </p>
+            </div>
+          </TooltipTrigger>
+          <TooltipContent
+            side="top"
+            align="center"
+            className="dark:bg-background dark:text-foreground p-3 font-bold"
+          >
+            <p>Total value of all your open positions.</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
   );
 };
 
