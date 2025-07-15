0fd614ab8e11c2c9fb4d53b205d987cb21f69da9
BoyanK95 <boyan_k_95@abv.bg>
2025-07-15 12:55:46 +0300

    fix: use shadcn btn component instead of normal btn

--- wealth-sync/src/components/Dashboard/AllPortfolioSummary/AllPortfolioSummary.tsx
+++ wealth-sync/src/components/Dashboard/AllPortfolioSummary/AllPortfolioSummary.tsx
@@ -23,6 +23,7 @@ import { calculateBinancePortfolioMetrics } from "../helper/calculateBinancePort
 import { findTopPerformingAsset } from "../helper/findTopPerformingAsset";
 import { Eye, EyeOff } from "lucide-react";
 import { Platforms } from "@/lib/constants/platforms";
+import { Button } from "@/components/ui/button";
 
 const AllPortfolioSummary = ({
   showStats,
@@ -164,16 +165,17 @@ const AllPortfolioSummary = ({
     <Card>
       <div className="ml-5 flex items-center space-x-2">
         <h2 className="text-xl font-semibold">Portfolio Summary</h2>
-        <button
+        <Button
+          variant="ghost"
           onClick={() => setShowStats(!showStats)}
-          className="cursor-pointer text-gray-500 hover:text-gray-700"
+          className="cursor-pointer text-gray-400 dark:hover:text-gray-200"
         >
           {showStats ? (
             <Eye className="h-6 w-6" />
           ) : (
             <EyeOff className="h-6 w-6" />
           )}
-        </button>
+        </Button>
       </div>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
         <Card>
