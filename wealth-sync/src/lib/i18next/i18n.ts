a81d56c01a9d4a3a5d84c3dae0e08fe481922a16
BoyanK95 <boyan_k_95@abv.bg>
2026-04-17 03:38:40 +0300

    fix: eslint and ts promise warning

--- wealth-sync/src/lib/i18next/i18n.ts
+++ wealth-sync/src/lib/i18next/i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from "react-i18next";
 import en from "../messages/en.json";
 import bg from "../messages/bg.json";
 
-i18n.use(initReactI18next).init({
+void i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     bg: { translation: bg },
