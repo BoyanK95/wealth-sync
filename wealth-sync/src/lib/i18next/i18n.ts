50e717465e7170f57e75bd213c5fb7b591850404
BoyanK95 <boyan_k_95@abv.bg>
2026-04-17 03:48:21 +0300

    feat: add i18next to root layout create bg en json for IntroductionSection

--- wealth-sync/src/lib/i18next/i18n.ts
+++ wealth-sync/src/lib/i18next/i18n.ts
@@ -1,28 +1,24 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "../messages/en.json";
 import bg from "../messages/bg.json";
 
 if (!i18n.isInitialized) {
-  void i18n
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-      resources: {
-        en: { translation: en },
-        bg: { translation: bg },
-      },
-      fallbackLng: "en",
-      interpolation: {
-        escapeValue: false,
-      },
-      detection: {
-        order: ["localStorage", "navigator"],
-        caches: ["localStorage"],
-      },
-    });
+  void i18n.use(LanguageDetector).init({
+    resources: {
+      en: { translation: en },
+      bg: { translation: bg },
+    },
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+  });
 }
 
 export default i18n;
