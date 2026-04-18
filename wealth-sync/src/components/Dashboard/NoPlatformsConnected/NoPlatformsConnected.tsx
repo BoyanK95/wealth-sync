589a144da950de06b6702aa968225b1091894dd1
BoyanK95 <boyan_k_95@abv.bg>
2026-04-19 02:59:19 +0300

    feat: translate no platnforms connected

--- wealth-sync/src/components/Dashboard/NoPlatformsConnected/NoPlatformsConnected.tsx
+++ wealth-sync/src/components/Dashboard/NoPlatformsConnected/NoPlatformsConnected.tsx
@@ -1,13 +1,17 @@
+'use client";';
+
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import IntegrationPlatformsGrid from "@/components/IntegrationPlatformsSection/IntegrationPlatformsGrid";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 const NoPlatformsConnected = () => {
+  const t = useTranslations("NoPlatformsConnected");
   return (
     <Card className="mt-7 w-full text-center">
       <CardHeader>
-        <CardTitle className="bold text-2xl">No Platforms Connected</CardTitle>
+        <CardTitle className="bold text-2xl">{t("title")}</CardTitle>
       </CardHeader>
       <CardContent className="space-y-6">
         <Image
@@ -18,17 +22,12 @@ const NoPlatformsConnected = () => {
           className="mx-auto"
         />
         <div className="text-muted-foreground text-xl">
-          <p>
-            You haven&apos;t connected any investment platforms yet. Connect
-            your first platform to start tracking your investments!
-          </p>
+          <p>{t("description")}</p>
         </div>
 
         <div className="items-center justify-center space-y-3">
-          <h3 className="text-xl font-medium">Available Platforms</h3>
-          <p className="text-muted-foreground">
-            Plase chose your prefered platform and click it to connect.
-          </p>
+          <h3 className="text-xl font-medium">{t("availablePlatforms")}</h3>
+          <p className="text-muted-foreground">{t("preferedPlatforms")}</p>
           <IntegrationPlatformsGrid />
         </div>
       </CardContent>
