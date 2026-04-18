279b0f5606284d3b89ca0af1df517ade2fe06523
BoyanK95 <boyan_k_95@abv.bg>
2026-04-18 23:44:43 +0300

    feat: translate navbar links

--- wealth-sync/src/components/Navbar/NavbarLinks.tsx
+++ wealth-sync/src/components/Navbar/NavbarLinks.tsx
@@ -1,27 +1,30 @@
 import { Routes } from "@/lib/constants/routes";
+import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 import React from "react";
 
-const NavbarLinks = () => {
+const NavbarLinks = async () => {
+  const t = await getTranslations("NavbarLinks");
+
   return (
     <div className="hidden items-center space-x-6 md:flex">
       <Link
         href={Routes.FEATURES}
         className="text-muted-foreground hover:text-foreground text-sm font-medium transition-colors"
       >
-        Features
+        {t("features")}
       </Link>
       <Link
         href={Routes.INTEGRATIONS}
         className="text-muted-foreground hover:text-foreground text-sm font-medium transition-colors"
       >
-        Integrations
+        {t("integrations")} 
       </Link>
       <Link
         href={Routes.PRICING}
         className="text-muted-foreground hover:text-foreground text-sm font-medium transition-colors"
       >
-        Pricing
+        {t("pricing")}
       </Link>
     </div>
   );
