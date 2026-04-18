1fdc63bd8b5db2c8fb29ad764855028b4edecdca
BoyanK95 <boyan_k_95@abv.bg>
2026-04-18 20:29:36 +0300

    feat: add translations to LangSwitchButton

--- wealth-sync/src/components/LangSwitchButton/LangSwitchButton.tsx
+++ wealth-sync/src/components/LangSwitchButton/LangSwitchButton.tsx
@@ -9,9 +9,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { Globe } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export default function LangSwitchButton() {
   const router = useRouter();
+  const t = useTranslations("LangSwitchButton");
 
   const changeLanguage = (locale: string) => {
     document.cookie = `locale=${locale}; path=/; max-age=31536000`;
@@ -24,16 +26,22 @@ export default function LangSwitchButton() {
       <DropdownMenuTrigger asChild>
         <Button variant="outline">
           <Globe className="h-4 w-4" />
-          <span className="ml-2">Language</span>
+          <span className="ml-2">{t("language")}</span>
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end">
-        <DropdownMenuItem className="cursor-pointer" onClick={() => changeLanguage("en")}>
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onClick={() => changeLanguage("en")}
+        >
           En
         </DropdownMenuItem>
 
-        <DropdownMenuItem className="cursor-pointer" onClick={() => changeLanguage("bg")}>
+        <DropdownMenuItem
+          className="cursor-pointer"
+          onClick={() => changeLanguage("bg")}
+        >
           BG
         </DropdownMenuItem>
       </DropdownMenuContent>
