2132d13bfb6e5b9f6e2ff02da9a45acadee39c73
BoyanK95 <boyan_k_95@abv.bg>
2026-04-20 20:59:21 +0300

    chore: add comment

--- wealth-sync/src/components/Dashboard/AllPortfolioSummary/components/MonthlyChangeSection.tsx
+++ wealth-sync/src/components/Dashboard/AllPortfolioSummary/components/MonthlyChangeSection.tsx
@@ -19,6 +19,7 @@ export const MonthlyChangeSection = ({ showStats }: { showStats: boolean }) => {
             <div className="text-2xl font-bold">+4.3%</div>
             <div className="flex items-center pt-1">
               <ArrowUp className="mr-1 h-4 w-4 text-green-700" />
+              {/* TODO replace with real monthly change value and style accordingly (green for positive, red for negative) */}
               <span className="text-green-700">+$5,120.45</span>
             </div>
           </>
