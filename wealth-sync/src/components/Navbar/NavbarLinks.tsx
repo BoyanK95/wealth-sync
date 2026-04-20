d244454ebd87d786c6e16dc2a71da3ec50fe5a77
BoyanK95 <boyan_k_95@abv.bg>
2026-04-20 22:13:33 +0300

    feat: add go to dashboard btn in navbar

--- wealth-sync/src/components/Navbar/NavbarLinks.tsx
+++ wealth-sync/src/components/Navbar/NavbarLinks.tsx
@@ -1,10 +1,12 @@
 import { Routes } from "@/lib/constants/routes";
+import { auth } from "@/server/auth";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 import React from "react";
 
 const NavbarLinks = async () => {
   const t = await getTranslations("NavbarLinks");
+  const session = await auth();
 
   return (
     <div className="hidden items-center space-x-6 md:flex">
@@ -18,7 +20,7 @@ const NavbarLinks = async () => {
         href={Routes.INTEGRATIONS}
         className="text-muted-foreground hover:text-foreground text-sm font-medium transition-colors"
       >
-        {t("integrations")} 
+        {t("integrations")}
       </Link>
       <Link
         href={Routes.PRICING}
@@ -26,6 +28,14 @@ const NavbarLinks = async () => {
       >
         {t("pricing")}
       </Link>
+      {session?.user && (
+        <Link
+          className="text-muted-foreground text-xl hover:shadow w-full hover:text-green-700 dark:hover:text-green-600"
+          href={Routes.DASHBOARD}
+        >
+          {t("goToDashboard")}
+        </Link>
+      )}
     </div>
   );
 };
