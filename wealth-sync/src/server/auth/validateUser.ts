b12e50f759435a637fb84a812fdce78c8175ed99
BoyanK95 <boyan_k_95@abv.bg>
2026-04-26 13:13:04 +0300

    refacgtor: validateUser with custom error class

--- wealth-sync/src/server/auth/validateUser.ts
+++ wealth-sync/src/server/auth/validateUser.ts
@@ -1,19 +1,22 @@
 import bcrypt from "bcryptjs";
 import { db } from "../db";
+export class InvalidCredentialsError extends Error {
+  constructor() {
+    super("Invalid credentials");
+    this.name = "InvalidCredentialsError";
+  }
+}
 
 export async function validateUser(email: string, password: string) {
   const user = await db.user.findUnique({
     where: { email },
   });
-
-  if (!user?.password) throw new Error('User does not have a password!');
-
+  if (!user?.password) throw new InvalidCredentialsError();
   try {
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) throw new Error('Password is invalid!');
+    if (!isPasswordValid) throw new InvalidCredentialsError();
   } catch (error) {
-    throw new Error(error as string);
+    throw error;
   }
-
   return user;
 }
