48bedfb0f6f6e4acf640a5bfd20fc59a7c757a7a
BoyanK95 <boyan_k_95@abv.bg>
2026-05-18 01:27:46 +0300

    feat: create initial NewsPage

---
+++ wealth-sync/src/app/news/page.tsx
@@ -0,0 +1,10 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "News | WealthSync",
+  description: "Stay updated with the latest news from a given ticker",
+};
+
+export default function NewsPage() {
+  return <div>page</div>;
+}
