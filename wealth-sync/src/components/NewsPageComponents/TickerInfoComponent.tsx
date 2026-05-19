c2ff1ef91f5e6cb45fcd51728b36e8ce021c1d9c
BoyanK95 <boyan_k_95@abv.bg>
2026-05-20 01:48:10 +0300

    fix: add translation to TickerInfoComponent

--- wealth-sync/src/components/NewsPageComponents/TickerInfoComponent.tsx
+++ wealth-sync/src/components/NewsPageComponents/TickerInfoComponent.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   Card,
@@ -20,11 +20,11 @@ export default function TickerInfoComponent({
   return (
     <Card className="rounded-3xl border border-gray-200 bg-white/80 p-6 shadow-sm dark:border-gray-700 dark:bg-slate-900/80">
       <CardHeader className="text-xl font-semibold">
-        <CardTitle>{result.name ?? result.symbol ?? "Asset info"}</CardTitle>
+        <CardTitle>{result.name ?? result.symbol ?? t("title")}</CardTitle>
         {result.logo && (
           <Image
             src={result.logo ?? "/default-image.png"}
-            alt={result.name ?? result.symbol ?? "Asset image"}
+            alt={result.name ?? result.symbol ?? t("assetLogo")}
             width={64}
             height={64}
             className="ml-4 rounded-full"
@@ -46,24 +46,24 @@ export default function TickerInfoComponent({
                   result.quote.d < 0 ? "text-red-600" : "text-green-600"
                 }`}
               >
-                Daily % change :{result.quote.d > 0 ? "+" : ""}
+                {t("dailyChange")}: {result.quote.d > 0 ? "+" : ""}
                 {result.quote.d} ({result.quote.dp?.toFixed(2)}%)
               </p>
             )}
             {result.quote.h && result.quote.l && (
               <p className="text-sm text-slate-500">
-                High: ${result.quote.h.toFixed(2)} | Low: $
+                {t("high")}: ${result.quote.h.toFixed(2)} | {t("low")}: $
                 {result.quote.l.toFixed(2)}
               </p>
             )}
             {result.quote.o && result.quote.o !== null && (
               <p className="text-sm text-slate-500">
-                Open: ${result.quote.o.toFixed(2)}
+                {t("open")}: ${result.quote.o.toFixed(2)}
               </p>
             )}
             {result.quote.pc && result.quote.pc !== null && (
               <p className="text-sm text-slate-500">
-                Previous Close: ${result.quote.pc.toFixed(2)}
+                {t("previousClose")}: ${result.quote.pc.toFixed(2)}
               </p>
             )}
           </div>
