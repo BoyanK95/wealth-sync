1b4b8a62f622a782ba5469e6c8392bda26e69dd3
BoyanK95 <boyan_k_95@abv.bg>
2026-05-24 23:42:31 +0300

    feat: add see recent news button in ErrorComponent

--- wealth-sync/src/components/NewsPageComponents/ErrorComponent.tsx
+++ wealth-sync/src/components/NewsPageComponents/ErrorComponent.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import { IoIosWarning } from "react-icons/io";
+import { Button } from "../ui/button";
+import { useTranslations } from "next-intl";
 
 function ErrorComponent({ error }: { error: string }) {
+  const t = useTranslations("NewsPage.ErrorComponent");
+
   return (
     <div
       role="alert"
       aria-live="assertive"
-      className="mt-10 rounded-3xl border border-gray-200 bg-white/80 p-6 shadow-sm dark:border-gray-700 dark:bg-slate-900/80"
+      className="mt-10 flex flex-col rounded-3xl border border-gray-200 bg-white/80 p-6 shadow-sm dark:border-gray-700 dark:bg-slate-900/80"
     >
       <IoIosWarning className="text-destructive mx-auto mt-6 text-6xl" />
       <p className="text-destructive text-md pt-2 text-center">{error}</p>
+      <Button size={"default"} variant={"outline"} className="mt-5 p-5">
+        {t("seeRecentNewsButton")}
+      </Button>
     </div>
   );
 }
