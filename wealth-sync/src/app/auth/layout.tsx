12276da0fc91b0efd398fe84d0cd01ae01f5fa35
BoyanK95 <boyan_k_95@abv.bg>
2026-06-10 23:23:33 +0300

    refactor: style layout and delete userNav

--- wealth-sync/src/app/auth/layout.tsx
+++ wealth-sync/src/app/auth/layout.tsx
@@ -15,9 +15,9 @@ export default async function AuthLayout({
   }
 
   return (
-    <div className="flex min-h-screen flex-col">
-      <main className="flex flex-1">
-        <div className="flex min-h-screen flex-1 flex-col justify-center bg-gradient-to-b from-[#66e37d] to-[#1f4923] px-6 py-12 lg:px-8 dark:bg-gradient-to-b dark:from-[#409e55] dark:to-[#000f01]">
+    <div className="flex min-h-screen flex-col pt-15">
+      <main className="flex-1">
+        <div className="min-h-screen flex-1 flex-col justify-center bg-gradient-to-b from-[#66e37d] to-[#1f4923] px-6 py-12 lg:px-8 dark:bg-gradient-to-b dark:from-[#409e55] dark:to-[#000f01]">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <div className="flex justify-center text-2xl font-bold tracking-tight text-white">
               <Link href={Routes.HOME} className="flex items-center gap-2">
